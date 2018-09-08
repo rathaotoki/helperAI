bff89a27f106d6c3472066f6b2fc3f00cdbd3bca
rathaotoki <d.s.cellratha@gmail.com>
2018-09-08 15:15:07 +0900

    a bit change for echo
    echo server

--- main.go
+++ main.go
@@ -6,9 +6,33 @@ import (
 	"os"
 )
 
+//AI core structure
+type AI struct {
+	name string
+}
+
+//Input functoin of AI.
+//tentatively only for string value.
+func (ai AI) Input(value string) {
+	fmt.Println(value)
+}
+
+func (ai AI) greet() {
+	fmt.Println("Hello, I am", ai.name, ".")
+}
+
+//NewAI creates a instance of AI structure
+func NewAI(name string) *AI {
+	ai := new(AI)
+	ai.name = name
+	return ai
+}
+
 func main() {
 	stdin := bufio.NewScanner(os.Stdin)
-
+	//var ai *AI
+	ai := NewAI("Yui")
+	ai.greet()
 	for stdin.Scan() {
 		text := stdin.Text()
 		fmt.Println(text)
